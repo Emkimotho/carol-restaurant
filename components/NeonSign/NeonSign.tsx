9a97a3daccde27ff98dc07e7189a9c6a38065674
Emilio <ajiravet@gmail.com>
2025-03-17 23:04:52 -0400

    neon

--- components/NeonSign/NeonSign.tsx
+++ components/NeonSign/NeonSign.tsx
@@ -34,7 +34,7 @@ const NeonSign: React.FC = () => {
           // Restaurant will open later today
           setStatus({
             isOpen: false,
-            message: `Opens today at ${convertTo12Hour(todayHours.open)}`,
+            message: `Will opens on today at ${convertTo12Hour(todayHours.open)}`,
           });
           return;
         } else if (currentTime >= openTime && currentTime < closeTime) {
