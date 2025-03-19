d34f07006cd701528b3b310a9d76528287393d7b
Emilio <ajiravet@gmail.com>
2025-03-18 22:17:47 -0400

    fixed error on fcart

--- app/menu/pageMainbackup.tsx
+++ app/menu/pageMainbackup.tsx
@@ -1,10 +1,7 @@
-
 "use client";
-
 
 import dynamic from "next/dynamic";
 import MenuComponent from "@/components/Menu/Menu";
-
 
 interface MenuPageProps {
   params: {
@@ -12,28 +9,19 @@ interface MenuPageProps {
   };
 }
 
-
-// Dynamically import components that should only render on the client side.
-const SidebarCart = dynamic(() => import("@/components/SidebarCart/SidebarCart"), {
-  ssr: false,
-});
-const FloatingCartBar = dynamic(() => import("@/components/FloatingCartBar/FloatingCartBar"), {
-  ssr: false,
-});
-
+// Dynamically import SidebarCart for client-side rendering.
+const SidebarCart = dynamic(
+  () => import("@/components/SidebarCart/SidebarCart"),
+  { ssr: false }
+);
 
 export default function MenuPage({ params }: MenuPageProps) {
   const slug = params.slug ?? [];
 
-
   return (
     <>
       <MenuComponent slug={slug} />
-      
-      <FloatingCartBar />
+      {/* FloatingCartBar removed */}
     </>
   );
 }
-
-
-
