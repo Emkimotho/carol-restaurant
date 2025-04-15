e3c6a5c19e2e8fcab31f976a369f2d82835ea945
Emilio <ajiravet@gmail.com>
2025-04-14 22:58:06 -0400

    reservation form in small

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -1,6 +1,6 @@
 
 // app/reservation/page.tsx
-import ReservationForm from "@/components/forms/ReservationForm";
+import ReservationForm from "@/components/forms/Reservationform";
 export default function ReservationPage() {
   return <ReservationForm />;
 }
