52474310dfd150d8a9469ed0869d00d52d40babb
Emilio <ajiravet@gmail.com>
2025-04-14 23:11:34 -0400

    res

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -1,6 +1,6 @@
 
 // app/reservation/page.tsx
-import ReservationForm from "@/components/forms/Reservationform";
+import ReservationForm from "@/components/forms/ReservationForm";
 export default function ReservationPage() {
   return <ReservationForm />;
 }
