ce157e4cbf1970f60c8b004c1785616c09fd4ae8
Emilio <ajiravet@gmail.com>
2025-04-14 22:43:29 -0400

    routes

--- app/catering/page.tsx
+++ app/catering/page.tsx
@@ -1,5 +1,5 @@
 // app/catering/page.tsx
-import CateringForm from "@/components/forms/cateringform";
+import CateringForm from "@/components/forms/CateringForm";
 export default function CateringPage() {
   return <CateringForm />;
 }
